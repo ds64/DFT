3ff6a5866b9b36671dd6c57df635b017f25fd697
playingg0d <wnmplayinggod@gmail.com>
2014-12-22 22:26:26 +0300

    Initial Commit

---
+++ FFT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FFT
+//
+//  Created by Andrey Bogdanov on 22.12.14.
+//  Copyright (c) 2014 Andrey Bogdanov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
